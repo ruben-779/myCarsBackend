945482f6258fbbbcd1395b13ee8b2892835ff465
ruben-779 <ruben_1995_fcv@hotmail.com>
2021-07-12 11:33:23 +0100

    finalize router and listen port

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ app.use(express.json());
 //to connect to mongodb
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/appCars");
+
+//router
+const routerUsers = require("./api/users/users.router");
+const routerCars = require("./api/cars/cars.router");
+const routerDealerships = require("./api/dealerships/dealerships.router");
+const routerAuth = require("./auth/auth.router");
+
+app.use("/users", routerUsers);
+app.use("/cars", routerCars);
+app.use("/dealerships", routerDealerships);
+app.use("/", routerAuth);
+
+//listen port
+app.listen(3000, () => console.log("ready at port 3000"));
