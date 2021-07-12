4458320ba8ef05c0f4fcd781f93cc99286130197
ruben-779 <ruben_1995_fcv@hotmail.com>
2021-07-12 18:42:36 +0100

    fix bug

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -11,7 +11,7 @@ module.exports = {
 };
 
 function deleteCarsUser(id) {
-  return userModel.updateMany(
+  return usersModel.updateMany(
     { favouriteCars: id },
     { $pull: { favouriteCars: id } }
   );
